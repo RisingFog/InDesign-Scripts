0754121ca7881b65e0c9124dc51fcfbc8678f1e0
RisingFog <christhecoolist@gmail.com>
2019-10-20 02:52:46 -0400

    Move file type check inside populateList function

--- lettererbuddy.jsx
+++ lettererbuddy.jsx
@@ -4,143 +4,137 @@ var doc = app.activeDocument;
 var script = [];
 var scriptFile = null;
 var scriptFileName = null;
+
 openScript();
+readScript();
 
-if (scriptFile != "" && scriptFile != null && scriptFileName.indexOf(".txt") !== -1) {
-    readScript();
+// DIALOG
+// ======
+var dialog = new Window("palette"); 
+    dialog.text = "Letterer Buddy"; 
+    dialog.orientation = "column"; 
+    dialog.alignChildren = ["center","top"]; 
+    dialog.spacing = 10; 
+    dialog.margins = 16; 
+    dialog.onClose = function() {
+        doc.removeEventListener('afterSelectionChanged', selectionChanged);
+    }
 
-    // DIALOG
-    // ======
-    var dialog = new Window("palette"); 
-        dialog.text = "Letterer Buddy"; 
-        dialog.orientation = "column"; 
-        dialog.alignChildren = ["center","top"]; 
-        dialog.spacing = 10; 
-        dialog.margins = 16; 
-        dialog.onClose = function() {
-            doc.removeEventListener('afterSelectionChanged', selectionChanged);
+// SCRIPTPANEL
+// ===========
+var scriptPanel = dialog.add("panel", undefined, undefined, {name: "scriptPanel"}); 
+    scriptPanel.text = "Script"; 
+    scriptPanel.orientation = "column"; 
+    scriptPanel.alignChildren = ["left","top"]; 
+    scriptPanel.spacing = 10; 
+    scriptPanel.margins = 10; 
+
+var list = scriptPanel.add("listbox", undefined, undefined, {name: "list"}); 
+    list.preferredSize.width = 300; 
+    list.preferredSize.height = 150; 
+    populateList() 
+
+// SETTINGSPANEL
+// =============
+var settingsPanel = dialog.add("panel", undefined, undefined, {name: "settingsPanel"}); 
+    settingsPanel.text = "Settings"; 
+    settingsPanel.orientation = "row"; 
+    settingsPanel.alignChildren = ["left","top"]; 
+    settingsPanel.spacing = 10; 
+    settingsPanel.margins = 10; 
+
+// OPTIONSPANEL
+// ============
+var optionsPanel = settingsPanel.add("panel", undefined, undefined, {name: "optionsPanel"}); 
+    optionsPanel.text = "Options"; 
+    optionsPanel.orientation = "column"; 
+    optionsPanel.alignChildren = ["left","top"]; 
+    optionsPanel.spacing = 10; 
+    optionsPanel.margins = 10; 
+
+var speakerText = optionsPanel.add("checkbox", undefined, undefined, {name: "speakerText"}); 
+    speakerText.text = "Remove Speaker Text"; 
+    speakerText.onClick = function() {
+    if (list != null) {
+        if (speakerText.value) {
+            var regex = /^\w+:\s?/
+            for (var i=0; i<script.length; i++) {
+                if (script[i].match(regex)) {
+                    script[i] = script[i].replace(regex, "");
+                }
+            }
         }
+        populateList();
+    }
+};
 
-    // SCRIPTPANEL
-    // ===========
-    var scriptPanel = dialog.add("panel", undefined, undefined, {name: "scriptPanel"}); 
-        scriptPanel.text = "Script"; 
-        scriptPanel.orientation = "column"; 
-        scriptPanel.alignChildren = ["left","top"]; 
-        scriptPanel.spacing = 10; 
-        scriptPanel.margins = 10; 
-
-    var list = scriptPanel.add("listbox", undefined, undefined, {name: "list"}); 
-        list.preferredSize.width = 300; 
-        list.preferredSize.height = 150; 
+var crossbarI = optionsPanel.add("checkbox", undefined, undefined, {name: "crossbarI"}); 
+    crossbarI.text = "Replace Crossbar I"; 
+    crossbarI.onClick = function() {
+    if (list != null) {
+        if (crossbarI.value == 1) {
+            var regex = /\bI\B/
+            for (var i=0; i<script.length; i++) {
+                if (script[i].match(regex)) {
+                    script[i] = script[i].replace(regex, "i");
+                }
+            }
+        }
         populateList() 
+    }
+};
 
-    // SETTINGSPANEL
-    // =============
-    var settingsPanel = dialog.add("panel", undefined, undefined, {name: "settingsPanel"}); 
-        settingsPanel.text = "Settings"; 
-        settingsPanel.orientation = "row"; 
-        settingsPanel.alignChildren = ["left","top"]; 
-        settingsPanel.spacing = 10; 
-        settingsPanel.margins = 10; 
-
-    // OPTIONSPANEL
-    // ============
-    var optionsPanel = settingsPanel.add("panel", undefined, undefined, {name: "optionsPanel"}); 
-        optionsPanel.text = "Options"; 
-        optionsPanel.orientation = "column"; 
-        optionsPanel.alignChildren = ["left","top"]; 
-        optionsPanel.spacing = 10; 
-        optionsPanel.margins = 10; 
-
-    var speakerText = optionsPanel.add("checkbox", undefined, undefined, {name: "speakerText"}); 
-        speakerText.text = "Remove Speaker Text"; 
-        speakerText.onClick = function() {
-        if (list != null) {
-            if (speakerText.value) {
-                var regex = /^\w+:\s?/
-                for (var i=0; i<script.length; i++) {
-                    if (script[i].match(regex)) {
-                        script[i] = script[i].replace(regex, "");
-                    }
+var ellipses = optionsPanel.add("checkbox", undefined, undefined, {name: "ellipses"}); 
+    ellipses.text = "Replace Ellipses with Periods"; 
+    ellipses.onClick = function() {
+    if (list != null) {
+        if (ellipses.value == 1) {
+            for (var i=0; i<list.items.length; i++) {
+                if (script[i].match("…")) {
+                    script[i] = script[i].replace("…", "...");
                 }
             }
-            populateList();
         }
+        else if (ellipses.value == 0) {
+            for (var i=0; i<list.items.length; i++) {
+                if (script[i].match("...")) {
+                    script[i] = script[i].replace("...", "…");
+                }
+            }
+        }
+        populateList() 
+    }
+};
+
+// ACTIONSPANEL
+// ============
+var actionsPanel = settingsPanel.add("panel", undefined, undefined, {name: "actionsPanel"}); 
+    actionsPanel.text = "Actions"; 
+    actionsPanel.orientation = "column"; 
+    actionsPanel.alignChildren = ["left","top"]; 
+    actionsPanel.spacing = 10; 
+    actionsPanel.margins = 10; 
+
+var loadScript = actionsPanel.add("button", undefined, undefined, {name: "loadScript"}); 
+    loadScript.text = "Load Script"; 
+    loadScript.onClick = function() {
+        resetOptions();
+        openScript();
+        readScript();
+        populateList();
     };
 
-    var crossbarI = optionsPanel.add("checkbox", undefined, undefined, {name: "crossbarI"}); 
-        crossbarI.text = "Replace Crossbar I"; 
-        crossbarI.onClick = function() {
-        if (list != null) {
-            if (crossbarI.value == 1) {
-                var regex = /\bI\B/
-                for (var i=0; i<script.length; i++) {
-                    if (script[i].match(regex)) {
-                        script[i] = script[i].replace(regex, "i");
-                    }
-                }
-            }
-            populateList() 
-        }
+var resetScript = actionsPanel.add("button", undefined, undefined, {name: "resetScript"}); 
+    resetScript.text = "Reset Script"; 
+    resetScript.onClick = function() {
+        resetOptions();
+        readScript();
+        populateList();
     };
 
-    var ellipses = optionsPanel.add("checkbox", undefined, undefined, {name: "ellipses"}); 
-        ellipses.text = "Replace Ellipses with Periods"; 
-        ellipses.onClick = function() {
-        if (list != null) {
-            if (ellipses.value == 1) {
-                for (var i=0; i<list.items.length; i++) {
-                    if (script[i].match("…")) {
-                        script[i] = script[i].replace("…", "...");
-                    }
-                }
-            }
-            else if (ellipses.value == 0) {
-                for (var i=0; i<list.items.length; i++) {
-                    if (script[i].match("...")) {
-                        script[i] = script[i].replace("...", "…");
-                    }
-                }
-            }
-            populateList() 
-        }
-    };
+dialog.show();
 
-    // ACTIONSPANEL
-    // ============
-    var actionsPanel = settingsPanel.add("panel", undefined, undefined, {name: "actionsPanel"}); 
-        actionsPanel.text = "Actions"; 
-        actionsPanel.orientation = "column"; 
-        actionsPanel.alignChildren = ["left","top"]; 
-        actionsPanel.spacing = 10; 
-        actionsPanel.margins = 10; 
-
-    var loadScript = actionsPanel.add("button", undefined, undefined, {name: "loadScript"}); 
-        loadScript.text = "Load Script"; 
-        loadScript.onClick = function() {
-            resetOptions();
-            openScript();
-            readScript();
-            populateList();
-        };
-
-    var resetScript = actionsPanel.add("button", undefined, undefined, {name: "resetScript"}); 
-        resetScript.text = "Reset Script"; 
-        resetScript.onClick = function() {
-            resetOptions();
-            readScript();
-            populateList();
-        };
-
-    dialog.show();
-
-    doc.addEventListener('afterSelectionChanged', selectionChanged);
-
-}
-else if (scriptFile != "" && scriptFile != null && scriptFileName.indexOf(".txt") == -1){
-    alert("This file type is not supported. Please use .txt files only.");
-}
+doc.addEventListener('afterSelectionChanged', selectionChanged);
 
 function selectionChanged() {
     if (doc.selection[0] instanceof TextFrame && doc.selection[0].contents == '') {
@@ -160,6 +154,10 @@ function placeText() {
 }
 
 function populateList() {
+    if (scriptFile != "" && scriptFile != null && scriptFileName.indexOf(".txt") == -1){
+        alert("This file type is not supported. Please use .txt files only.");
+        return;
+    }
     list.removeAll();
     for (var i=0; i<script.length; i++) {
         list.add('item', script[i]);
